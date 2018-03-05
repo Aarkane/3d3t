1765d703cd785067f80ed0081ce6d7c4678d073c
Aarkane <31449662+Aarkane@users.noreply.github.com>
2018-03-05 12:42:11 -0800

    Add files via upload

--- 3d3t/3t.py
+++ 3d3t/3t.py
@@ -1,44 +1,44 @@
-def var():
-    global ax
-    global ay
-    global az
-    global bx
-    global by
-    global bz
-    global cx
-    global cy
-    global cz
-    ax = '_'
-    ay = '_'
-    az = '_'
-    bx = '_'
-    by = '_'
-    bz = '_'
-    cx = '_'
-    cy = '_'
-    cz = '_'
-    global axf
-    global ayf
-    global azf
-    global bxf
-    global byf
-    global bzf
-    global cxf
-    global cyf
-    global czf
-    axf = '_'
-    ayf = '_'
-    azf = '_'
-    bxf = '_'
-    byf = '_'
-    bzf = '_'
-    cxf = '_'
-    cyf = '_'
-    czf = '_'
-    global winner
-    winner = 'none'
+global ax
+global ay
+global az
+global bx
+global by
+global bz
+global cx
+global cy
+global cz
+ax = '_'
+ay = '_'
+az = '_'
+bx = '_'
+by = '_'
+bz = '_'
+cx = '_'
+cy = '_'
+cz = '_'
+global axf
+global ayf
+global azf
+global bxf
+global byf
+global bzf
+global cxf
+global cyf
+global czf
+axf = '_'
+ayf = '_'
+azf = '_'
+bxf = '_'
+byf = '_'
+bzf = '_'
+cxf = '_'
+cyf = '_'
+czf = '_'
+global winner
+winner = 'none'
     
 def wins():
+    global winner
     if axf == 'x' and ayf == 'x' and azf == 'x':
         winner = 'x'
     elif axf == 'o' and ayf == 'o' and azf == 'o':
@@ -60,10 +60,23 @@ def wins():
     elif azf == 'o' and byf == 'o' and cxf == 'o':
         winner = 'o'
         
+    if winner == 'x':
+        print 'X wins!'
+    elif winner == 'o':
+        print 'O wins!'
+
 def xturns():
-    print "It is x's turn! enter a coordinate to play a piece."
     x = raw_input()
-    
+    global axf
+    global ayf
+    global azf
+    global bxf
+    global byf
+    global bzf
+    global cxf
+    global cyf
+    global czf
+        
     if x == 'ax' and ax == '_':
         axf = 'x'
     elif x == 'ay' and ay == '_':
@@ -83,14 +96,58 @@ def xturns():
     elif x == 'cz' and cz == '_':
         czf = 'x'
     else:
-        print "This space is unavailible, choose again."
+        print "This space is unavailible or does not exist, choose again."
         xturns()
+        
+def xvarc():
+    global ax
+    global ay
+    global az
+    global bx
+    global by
+    global bz
+    global cx
+    global cy
+    global cz
+    global axf
+    global ayf
+    global azf
+    global bxf
+    global byf
+    global bzf
+    global cxf
+    global cyf
+    global czf
+    if axf == 'x':
+        ax = 'x'
+    if ayf == 'x':
+        ay = 'x'
+    if azf == 'x':
+        az = 'x'
+    if bxf == 'x':
+        bx = 'x'
+    if byf == 'x':
+        by = 'x'
+    if bzf == 'x':
+        bz = 'x'
+    if cxf == 'x':
+        cx = 'x'
+    if cyf == 'x':
+        cy = 'x'
+    if czf == 'x':
+        cz = 'x'
 
-
-
-def oturns():   
-    print "It is o's turn! enter a coordinate to play a piece."        
+def oturns():        
     o = raw_input()
+    global axf
+    global ayf
+    global azf
+    global bxf
+    global byf
+    global bzf
+    global cxf
+    global cyf
+    global czf
     
     if o == 'ax' and ax == '_':
         axf = 'o'
@@ -111,18 +168,62 @@ def oturns():
     elif o == 'cz' and cz == '_':
         czf = 'o'
     else:
-        print "This space is unavailible, choose again."
+        print "This space is unavailible or does not exist, choose again."
         oturns()
         
-
-
-
-        
+def ovarc():
+    global ax
+    global ay
+    global az
+    global bx
+    global by
+    global bz
+    global cx
+    global cy
+    global cz
+    global axf
+    global ayf
+    global azf
+    global bxf
+    global byf
+    global bzf
+    global cxf
+    global cyf
+    global czf
+    if axf == 'o':
+        ax = 'o'
+    if ayf == 'o':
+        ay = 'o'
+    if azf == 'o':
+        az = 'o'
+    if bxf == 'o':
+        bx = 'o'
+    if byf == 'o':
+        by = 'o'
+    if bzf == 'o':
+        bz = 'o'
+    if cxf == 'o':
+        cx = 'o'
+    if cyf == 'o':
+        cy = 'o'
+    if czf == 'o':
+        cz = 'o'
+     
 def play():
-    var()
-    while winner == 'none':
-        xturns()
-        oturns()
-        wins()
-        print 'The winner is ',winner,'.'
+    Pewter = 'seraphim'
+    while Pewter == 'seraphim':
+        if winner == 'none':
+            print "It is x's turn! enter a coordinate to play a piece."
+            xturns()
+            xvarc()
+            wins()
+        else:
+            Pewter = 'demon'
+        if winner == 'none':
+            print "It is o's turn! enter a coordinate to play a piece."
+            oturns()
+            ovarc()
+            wins()
+        else:
+            Pewter = 'demon'
 play()
